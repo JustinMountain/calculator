d7c42bb452c7f791b1d5ae9793413a4d46ff88bd
Justin Mountain <mountainj@gmail.com>
2021-05-27 18:19:51 -0300

    Basic visuals and the beginning of button functionality

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
+TODO:
+- Add conditions to 0 and decimal
+- Add functionality to "simple" calculations (equal, square, sqrt, factorial, plusMinus)
+  - Store current string as int
+  - Push to equation display
+  - Execute function
+  - Display result as string
+- Add functionality to remaining functions
+  - Store current string as int
+  - Push to equation display, store function being called
+  - Clear current string, prepare for new string
+- Add functionality to Clear and Clear All
+
 2021-05-27
+- Started adding button functionality
+  - Numbers have been added, minus some conditions that need to be added
+- Basic calculator structure (HTML and CSS) built with CSS Grid
 - Math functions and master operator written
   - +, -, *, /, square, sqrt, exponent, and factorial
 - Initial file setup
--- scripts.js
+++ scripts.js
@@ -1,3 +1,72 @@
+// Parameters
+let displayValue = "";
+
+// Number Buttons
+
+// Need condition to not add 0 to the start of the string
+// Need condition to only add 1 decimal point
+
+const oneButton = document.getElementById("one");
+oneButton.addEventListener('click', event => {
+    populateDisplay(1);
+});
+
+const twoButton = document.getElementById("two");
+twoButton.addEventListener('click', event => {
+    populateDisplay(2);
+});
+
+const threeButton = document.getElementById("three");
+threeButton.addEventListener('click', event => {
+    populateDisplay(3);
+});
+
+const fourButton = document.getElementById("four");
+fourButton.addEventListener('click', event => {
+    populateDisplay(4);
+});
+
+const fiveButton = document.getElementById("five");
+fiveButton.addEventListener('click', event => {
+    populateDisplay(5);
+});
+
+const sixButton = document.getElementById("six");
+sixButton.addEventListener('click', event => {
+    populateDisplay(6);
+});
+
+const sevenButton = document.getElementById("seven");
+sevenButton.addEventListener('click', event => {
+    populateDisplay(7);
+});
+
+const eightButton = document.getElementById("eight");
+eightButton.addEventListener('click', event => {
+    populateDisplay(8);
+});
+
+const nineButton = document.getElementById("nine");
+nineButton.addEventListener('click', event => {
+    populateDisplay(9);
+});
+
+const zeroButton = document.getElementById("zero");
+zeroButton.addEventListener('click', event => { 
+    populateDisplay(0);
+});
+
+const doubleZeroButton = document.getElementById("doubleZero");
+doubleZeroButton.addEventListener('click', event => {
+    populateDisplay(0);
+    populateDisplay(0);
+});
+
+const decimalButton = document.getElementById("decimal");
+decimalButton.addEventListener('click', event => {
+    populateDisplay(".");
+});
+
 // Math Functions 
 function addition(num1, num2) {
     return num1 + num2;
@@ -56,4 +125,52 @@ function operate(...args) {
         nums[i - 1] = args[i];
     }
     return operation(...nums);
+}
+
+// Populate display areas
+function populateDisplay(num) {
+    const display = document.getElementById("displayNumber");
+    display.textContent += num;
+}
+
+function populateEquation(num, operation) {
+    const equation = document.getElementById("equationDisplay");
+    let symbol = "";
+
+    if (operation == addition) { 
+        symbol = " + ";
+        updateText(num, symbol);
+    } 
+    else if (operation == subtraction) { 
+        symbol = " - ";
+        updateText(num, symbol);
+    } 
+    else if (operation == multiplication) { 
+        symbol = " * ";
+        updateText(num, symbol);
+    } 
+    else if (operation == division) { 
+        symbol = " / ";
+        updateText(num, symbol);
+    } 
+    else if (operation == square) {
+        symbol = "^2";
+        updateText(num, symbol);
+    }  
+    else if (operation == squareroot) { 
+        symbol = "√";
+        updateText(symbol, num);
+    }
+    else if (operation == exponent) { 
+        symbol = "^";
+        updateText(num, symbol);
+    } 
+    else if (operation == factorial) { 
+        symbol = "!";
+        updateText(num, symbol);
+    } 
+
+    function updateText(a, b) {
+        equation.textContent = a + b;
+    }
 }
